9448fb5ff7a023cfd666d908dcee276b1803c981
RezaulRoman <Rezaulkroman@gmail.com>
2022-01-15 12:32:18 -0500

    first commit

---
+++ src/hw1Q2JavaBsics/MySelf.java
@@ -0,0 +1,17 @@
+package hw1Q2JavaBsics;
+
+/* I am creating multiple line comments
+ * project started with upper case
+ * package started with lower case
+ * class started with upper case
+ * 
+ * 
+ */
+
+public class MySelf {
+
+	public static void main(String[] arag) {
+		System.out.println("Rezaul Roman");
+
+	}
+}
---
+++ src/hw4Q2javaVariables/AboutmeTest.java
@@ -0,0 +1,35 @@
+package hw4Q2javaVariables;
+
+public class AboutmeTest {
+
+	public static void main(String[] args) {
+
+		AboutMe me = new AboutMe();
+		me.name = "Roman";
+		me.gender = 'M';
+		me.age = 28;
+		me.myStudentLoan = 30000;
+		me.myAnnualIncome = 500000000000l;
+		me.usCitizen = true;
+		me.myFatherAge = 100;
+		me.myHighSchoolGrade = 3.14f;
+		me.myAssociteGrade = 4.589805555;
+		me.aboutMe();
+
+		System.out.println("\nMy friend alex information below\n");
+
+		AboutMe me2 = new AboutMe();
+		me2.name = "Alex";
+		me2.gender = 'M';
+		me2.age = 30;
+		me2.myStudentLoan = 30000;
+		me2.myAnnualIncome = 500000000000l;
+		me2.usCitizen = false;
+		me2.myFatherAge = 60;
+		me2.myHighSchoolGrade = 2.14f;
+		me2.myAssociteGrade = 8.589805555;
+		me2.aboutMe();
+
+	}
+
+}
